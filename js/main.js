f2fc31632e47c43b9f56320dda2eb51439022e52
JoannaYEH <joanna883311@gmail.com>
2024-01-05 16:44:54 +0800

    20240105_01
    整體樣式
    日期斜線
    back to top
    掛號預約單 定位點
    細節修正

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function GetRegForm(DocInfo,RegDate,RegNoon,DocName,RegNum) {
    document.getElementById("DocName").innerHTML = '醫師姓名：'+ DocInfo.getElementsByTagName('p')[0].innerHTML;
    document.getElementById("DocDate").innerHTML = '看診日期：'+  GetDateRangeST(first_Week+7*wo+RegDate)+RegNoon;
    document.getElementById("Info_Date").innerHTML = '看診日期：'+ GetDateRangeST(first_Week+7*wo+RegDate);
-   document.getElementById("Info_Noon").innerHTML = '時間：'+ RegNoon;
+   document.getElementById("Info_Noon").innerHTML = '時段：'+ RegNoon;
    document.getElementById("Info_Department").innerHTML = '科別：'+ document.getElementById("Department").innerHTML;
    document.getElementById("Info_DocName").innerHTML = '看診醫師：'+ DocName;
    var _RegNum=RegNum+1;
@@ -57,9 +57,9 @@ function GetDateRangeST(_Date){
     var _day = new Date(_DT.setDate(_Date));
   
     // 本周第一天
-    var year = _day.toLocaleString("zh-TW", { year: "numeric" });
-    var month = _day.toLocaleString("zh-TW", { month: "short" });
-    var day = _day.toLocaleString("zh-TW", { day: "numeric" });
+    var year = _day.toLocaleString("zh-TW", { year: "numeric" }).replace('年','/');
+    var month = _day.toLocaleString("zh-TW", { month: "short" }).replace('月','/');
+    var day = _day.toLocaleString("zh-TW", { day: "numeric" }).replace('日','');;
   
     var formattedDate = [year, month, day].join('');
   
@@ -74,21 +74,40 @@ function GetDateRangeST(_Date){
   // Loop over them and prevent submission
   var validation = Array.prototype.filter.call(forms, function(form) {
     form.addEventListener('submit', function(event) {
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      else{
+      if (form.checkValidity() === true) {
+        // event.preventDefault();
+        // event.stopPropagation();
         $('#InfoModal').modal('show');
       }
-      
       //form.classList.add('was-validated');
     }, false);
   });
 }, false);
 })();
 
-$("#InfoModal").on("hide.bs.modal", function () {
+$("#InfoModal").on("hidden.bs.modal", function () {
   // location.reload(true);
-  location.href = location.href;
+  //location.href = location.href;
+  document.getElementById("reviewsSection").hidden=false;
+  document.getElementById("featuresSection").hidden=false;
+  document.getElementById("week_order").value=0
 });
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
